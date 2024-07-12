6609ecddeff7da6cff923fbf971db5a51792b574
aswinbennyofficial <aswinbenny.official@gmail.com>
2024-07-12 09:01:23 +0530

    added option to encrypt passphrase as well

--- db/machines.go
+++ db/machines.go
@@ -21,11 +21,22 @@ func SaveMachine(config models.Config, machine models.Machine, username string) 
 	log.Debug().Msg("SaveMachine() : Salt: "+salt)
 
 
-	enc_text,ivString,err:=utils.EncryptString(machine.PrivateKey, machine.Password, salt)
+	enc_PrivateKey,ivPrivateKey,err:=utils.EncryptString(machine.PrivateKey, machine.Password, salt)
 	if err != nil {
 		log.Error().Msgf("Error encrypting private key: %v", err)
 		return err
 	}
+
+	enc_Passphrase:=""
+	ivPassphrase:=""
+	if machine.Passphrase!=""{
+		enc_Passphrase, ivPassphrase, err = utils.EncryptString(machine.Passphrase, machine.Password, salt)
+		if err != nil {
+			log.Error().Msgf("Error encrypting passphrase: %v", err)
+			return err
+		}
+	}
+	
 
 
 	owner_id:=username
@@ -40,7 +51,7 @@ func SaveMachine(config models.Config, machine models.Machine, username string) 
 
 
 	// save machine to database
-	_, err = config.DB.Exec(context.Background(), "INSERT INTO machines (name, username, hostname, port, encrypted_private_key, encrypted_passphrase, iv, owner_id, owner_type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", machine.Name, machine.Username, machine.Hostname, machine.Port, enc_text, ivString, salt, owner_id, owner_type)
+	_, err = config.DB.Exec(context.Background(), "INSERT INTO machines (name, username, hostname, port, encrypted_private_key, iv_private_key, encrypted_passphrase, iv_passphrase, owner_id, owner_type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", machine.Name, machine.Username, machine.Hostname, machine.Port, enc_PrivateKey, ivPrivateKey, enc_Passphrase, ivPassphrase, owner_id, owner_type)
 	if err != nil {
 		log.Error().Msgf("Error saving machine to database: %v", err)
 		return err
--- models/server.go
+++ models/server.go
@@ -15,7 +15,9 @@ type Machine struct {
 	Hostname   string `json:"hostname"`
 	Port       string    `json:"port"`
 	PrivateKey string `json:"private_key"`
+    IvPrivateKey string `json:"iv_private_key"`
 	Passphrase string `json:"passphrase"`
+    IvPassphrase string `json:"iv_passphrase"`
     Password   string `json:"password"`
 	Organization string `json:"organization"`
 }
